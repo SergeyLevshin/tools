5d3552aaa5a9318cde2f8480f99da5d72e57cc6b
user.email <you@example.com>
2021-02-08 16:40:34 +0400

    propertycontroller and views changed

--- src/main/java/com/toolsapp/controller/PropertyController.java
+++ src/main/java/com/toolsapp/controller/PropertyController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -46,6 +47,27 @@ public class PropertyController {
     @PostMapping("/addProducer")
     public String addGroup(Producer producer) {
         producerService.save(producer);
+        return "redirect:/property/showToolProperties";
+    }
+
+    @GetMapping("/deleteToolProperty")
+    public String deleteProperty(Model model) {
+        List<Group> groups = groupService.findAll();
+        List<Producer> producers = producerService.findAll();
+        model.addAttribute("groups", groups);
+        model.addAttribute("producers", producers);
+        return "/property/deleteToolProperty";
+    }
+
+    @PostMapping("/deleteGroup")
+    public String deleteGroup(@Valid long id) {
+        groupService.deleteById(id);
+        return "redirect:/property/showToolProperties";
+    }
+
+    @PostMapping("/deleteProducer")
+    public String deleteProducer(@Valid long id) {
+        producerService.deleteById(id);
         return "redirect:/property/showToolProperties";
     }
 }
